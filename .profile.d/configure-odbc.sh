3484863d94ecf7249a739ee34993ba018333d65a
BHISupaMike <mcaldwell@bhico.com>
2025-01-13 16:07:37 -0700

    testing

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export ODBCSYSINI=${HOME}/.apt/usr/lib/odbc/conf/
+export ODBCINI=${HOME}/.apt/usr/lib/odbc/conf/odbc.ini
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/.apt/usr/lib
 
 mkdir -p /etc
 
@@ -8,4 +10,10 @@ echo "[ODBC Driver 18 for SQL Server]
 Description=Microsoft ODBC Driver 18 for SQL Server
 Driver=/app/.apt/usr/lib/libmsodbcsql-18.1.so.1.1
 UsageCount=1
+LoginTimeout=30
+ConnectRetryCount=3
+ConnectRetryInterval=5
+Encrypt=yes
+TrustServerCertificate=yes
+Mars_Connection=yes
 " > ${ODBCSYSINI}/odbc.ini
