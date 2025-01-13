3e76a174c72b96243474e0e2494bfea5d6c34e79
BHISupaMike <mcaldwell@bhico.com>
2025-01-13 15:26:50 -0700

    puts together an odbc 18 buildpack for heroku

---
+++ .profile.d/configure-odbc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ODBCSYSINI=${HOME}/.apt/usr/lib/odbc/conf/
+
+mkdir -p /etc
+
+echo "[ODBC Driver 18 for SQL Server]
+Description=Microsoft ODBC Driver 18 for SQL Server
+Driver=/app/.apt/usr/lib/libmsodbcsql-18.1.so.1.1
+UsageCount=1
+" > ${ODBCSYSINI}/odbc.ini
